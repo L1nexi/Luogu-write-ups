f214ef3c923947e178301dea5fcad90b3cc64cbf
L1nexi <l1nexi@outlook.com>
2023-09-11 23:19:29 +0800

    Submit

---
+++ test.py
@@ -0,0 +1,22 @@
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+ 
+def func(first, second):
+    print(first)
+    print(second)
+
+# 创建生成器对象
+generator = countdown(5)
+ 
+# 通过迭代生成器获取值
+print(next(generator))  # 输出: 5
+print(next(generator))  # 输出: 4
+print(next(generator))  # 输出: 3
+ 
+# 使用 for 循环迭代生成器
+for value in generator:
+    print(value)  # 输出: 2 1
+func(1, 2)
+func(second=1, first=2)
