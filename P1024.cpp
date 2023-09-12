24dc8035427f24374466577852a039c0d786e8d8
L1nexi <l1nexi@outlook.com>
2023-09-12 14:37:27 +0800

    P1024

--- P1024.cpp
+++ P1024.cpp
@@ -9,7 +9,47 @@ double binSolve(const std::vector<double> &poly, double left, double right);
 int main(void)
 {
 
-    // std::cout << fixed << setprecision(2) << ans[0] << ' ' << ans[1] << ' ' << ans[2] << endl;
+    std::vector<double> poly;
+    for (int i = 0; i < 4; ++i)
+    {
+        double tmp;
+        std::cin >> tmp;
+        poly.emplace_back(tmp);
+    }
+    double prev;
+    std::vector<double> points;
+    std::vector<double> ans;
+
+    double it = -200.0;
+    prev = func(poly, -200.5);
+    while (prev * func(poly, it) > 0.0)
+    {
+        it = it + 0.5;
+    }
+    points.emplace_back(it - 0.5);
+    it += 0.5;
+    prev = func(poly, it);
+    while (prev * func(poly, it) > 0.0)
+    {
+        it  = it + 0.5;
+    }
+    points.emplace_back(it - 0.5);
+    it += 0.5;
+    prev = func(poly, it);
+    while (prev * func(poly, it) > 0.0)
+    { 
+        it = it + 0.5;
+    }
+    points.emplace_back(it - 0.5);
+    points.emplace_back(it + 1);
+    for(int i{0}; i < 3; ++i)
+    {
+        ans.emplace_back(binSolve(poly, points[i], points[i + 1]));
+    }
+    std::cout << fixed << setprecision(2) << ans[0] << ' ' << ans[1] << ' ' << ans[2] << endl;
+
+    // std::vector<double> poly{1.0, -5.0, -4.0, 20};
+    // std::cout << binSolve(poly, -2.5, 1.5) << endl;
 
     return 0;
 }
@@ -22,9 +62,11 @@ double binSolve(const std::vector<double> &poly, double left, double right)
     double mid = (left + right) / 2;
     while (right - left > eps)
     {
-        if (func(poly, mid) * func(poly, left) < 0)
+        if (func(poly, mid) < eps && func(poly, mid) > -eps)
+            return mid;
+        if (func(poly, mid) * func(poly, left) <= 0)
             right = mid;
-        else
+        else if(func(poly, mid) * func(poly, right) <= 0)
             left = mid;
         mid = (left + right) / 2;
     }
